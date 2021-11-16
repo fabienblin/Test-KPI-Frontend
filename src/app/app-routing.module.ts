3e5a599918630bff3595af672d4ece695e379b44
fabienblin <fabien.blin@gmail.com>
2021-11-16 16:14:26 +0100

    routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InvestmentDetailComponent } from './investment-detail/investment-detail.component';
+import { InvestmentsComponent } from './investments/investments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'investments', component: InvestmentsComponent },
+	{ path: 'detail/:id', component: InvestmentDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { InvestmentDetailComponent } from './investment-detail/investment-detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestmentsComponent,
+    InvestmentDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
