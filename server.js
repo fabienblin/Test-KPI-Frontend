44f38e695a827024dc9578eea3df1099b2d75bb6
fabienblin <fabien.blin@gmail.com>
2021-11-17 18:43:42 +0100

    test env

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(__dirname + '/dist/kpi'));
-app.get('/*', function(req,res) {
+
+app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname + '/dist/kpi/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
